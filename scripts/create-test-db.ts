af3aaa9d59a8f33a4868adf7e5b87e0e153c9c8d
vlab-dev-m2 <csapuras@gmail.com>
2026-07-15 09:50:42 +0800

    test: add init test

--- scripts/create-test-db.ts
+++ scripts/create-test-db.ts
@@ -2,11 +2,15 @@ import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
-const createTestDB = () => {
+const createTestDb = async () => {
+  const testDB = Bun.file("test.sqlite");
+  if (await testDB.exists()) {
+    await Bun.file("test.sqlite").delete();
+  }
   const sqlite = new Database("test.sqlite");
   const db = drizzle(sqlite);
-  migrate(db, { migrationsFolder: "./src/server/db/migtrations" });
+  migrate(db, { migrationsFolder: "./src/server/db/migrations" });
   console.log("Test database created and migrated");
 };
 
-createTestDB();
+createTestDb();
---
+++ src/server/db/queries.test.ts
@@ -0,0 +1,38 @@
+import { it, expect, mock, beforeEach } from "bun:test";
+import { insertSubscriber } from "./queries";
+import type { D1Database } from "@cloudflare/workers-types";
+import type { NewSubscriber } from "./schema";
+import { getTestDb } from "../../../test/get-test-db";
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { reset } from "drizzle-seed";
+import * as schema from "./schema";
+
+mock.module("./db.ts", () => {
+  return {
+    getDB: () => getTestDb(),
+  };
+});
+
+beforeEach(async () => {
+  const db = getTestDb();
+  await reset(db, schema);
+});
+
+it("inserts a new subscrier into the database", async () => {
+  const newSub: NewSubscriber = { email: "test@test.com" };
+  const subscriber = await insertSubscriber({} as D1Database, newSub);
+  expect(subscriber.email).toBe(newSub.email);
+  expect(subscriber.id).toBeDefined();
+  expect(subscriber.createdAt).toBeDefined();
+});
+
+it("throws an error when inserting a duplicate email", async () => {
+  const newSub: NewSubscriber = { email: "test@test.com" };
+  await insertSubscriber({} as D1Database, newSub);
+  expect(insertSubscriber({} as D1Database, newSub)).rejects.toThrow();
+});
+
+it("throws an error when trying to insert an invalid email", async () => {
+  const newSub: NewSubscriber = { email: "test@test" };
+  expect(insertSubscriber({} as D1Database, newSub)).rejects.toThrow();
+});
